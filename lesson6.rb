31d9284fbd5d001e9715a00ccbedddf018130ae7
EC2 Default User <ec2-user@ip-172-31-6-191.ec2.internal>
2021-03-02 05:55:26 +0000

    finish 2-6

---
+++ lesson6.rb
@@ -0,0 +1,10 @@
+total_price = 60
+
+if total_price > 100
+  puts "みかんを購入。所持金が余りあり。"
+elsif total_price == 100
+  puts "みかんを購入。所持金は0円。"
+elsif total_price < 100
+  puts "みかんを購入する事が出来ませんでした。"
+end
+  
